ce5e0569d06c99b50fa6a49faf4fa15604c7f394
josepps <joseppsantanafs@gmail.com>
2022-10-23 14:13:28 -0300

    Página de feed criada

---
+++ src/Feed.style.ts
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    max-width: 1440px;
+    height: 10vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.08);
+
+
+    .logoHeader {
+        height: 10vh;
+        margin-left: 20%;
+    }
+
+    .navHeader {
+        margin-right: 7%;
+    }
+     
+    .navHeader a:nth-child(1) {
+        color: #76BB4C;
+        font-size: 1.6rem;
+        text-decoration: none;
+        margin-right: 10px;
+    }
+
+    .navHeader a:nth-child(2){
+        text-decoration: none;
+        color: #6033AA;
+        font-size: 1.2rem;
+    }
+`;
+
+export const Main =styled.main`
+    max-width: 1440px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .formPublicação {
+        width: 60%;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        border-width: 0px 1px 1px 1px;
+        border-style: solid;
+        border-color: #C5C5C5;
+        padding: 3% 2% 4% 2%;
+
+        form {
+            margin-left: 3%;
+            width: 80%;
+            position: relative;
+
+            input {
+                width: 100%;
+                min-height: 7vh;
+                border: 1px solid #8BC34A;
+                border-radius: 10px;
+            }
+
+            button {
+                background: #6033AA;
+                border-radius: 30px;
+                color: #fff;
+                border: none;
+                padding: 0.5% 5%;
+                position: absolute;
+                top: 120%;
+                right: 0;
+            }
+        }
+    }
+
+    .pulblicaçõesFeed {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        width: 60%;
+        border-width: 0px 1px 1px 1px;
+        border-style: solid;
+        border-color: #C5C5C5;
+        padding: 2% 2% 3% 2%;
+
+        div {
+            margin-left: 3%;
+            position: relative;
+
+            h4 {
+                color: #76BB4C;
+            }
+
+            span {
+                font-size: 0.8rem;
+                position: absolute;
+                top: 41%;
+                left: 0;
+                color: #919191;
+            }
+        }
+    }
+`;
---
+++ src/Feed.tsx
@@ -0,0 +1,67 @@
+import { Header, Main } from './Feed.style';
+
+
+function Feed() {
+
+  return (
+    <>
+      <Header>
+        <a href="#"><img className='logoHeader' src="../img/logoLorizontalColorido.png" alt="Logo da parrot" /></a>
+        <nav className='navHeader'>
+          <a href="#">Olá, usuário |</a>
+          <a href="#">sair</a>
+        </nav>
+      </Header>
+      <Main>
+        <section className='formPublicação'>
+          <img src="../img/iconeHomem.png" alt="Imagem de homem" />
+          <form>
+            <div>
+              <label htmlFor="Postagem"></label>
+              <input type="text" aria-describedby='Postagem' />
+            </div>
+            <button typeof='submit'>Pulblicar</button>
+          </form>
+        </section>
+        <section className='pulblicaçõesFeed'>
+          <img src="../img/iconeHomem.png" alt="" />
+          <div>
+            <h4>Vinicius - apê 42</h4>
+            <span>10/06/2022 09:00</span>
+            <p>Poxa galera, vamos respeitar o horário de silêncio!
+              Tinha alguém usando furadeira 2 HORAS DA MANHÃ!!! ó_ó</p>
+          </div>
+        </section>
+        <section className='pulblicaçõesFeed'>
+          <img src="../img/iconeHomem.png" alt="" />
+          <div>
+            <h4>Vinicius - apê 42</h4>
+            <span>10/06/2022 09:00</span>
+            <p>Poxa galera, vamos respeitar o horário de silêncio!
+              Tinha alguém usando furadeira 2 HORAS DA MANHÃ!!! ó_ó</p>
+          </div>
+        </section>
+        <section className='pulblicaçõesFeed'>
+          <img src="../img/iconeHomem.png" alt="" />
+          <div>
+            <h4>Vinicius - apê 42</h4>
+            <span>10/06/2022 09:00</span>
+            <p>Poxa galera, vamos respeitar o horário de silêncio!
+              Tinha alguém usando furadeira 2 HORAS DA MANHÃ!!! ó_ó</p>
+          </div>
+        </section>
+        <section className='pulblicaçõesFeed'>
+          <img src="../img/iconeHomem.png" alt="" />
+          <div>
+            <h4>Vinicius - apê 42</h4>
+            <span>10/06/2022 09:00</span>
+            <p>Poxa galera, vamos respeitar o horário de silêncio!
+              Tinha alguém usando furadeira 2 HORAS DA MANHÃ!!! ó_ó</p>
+          </div>
+        </section>
+      </Main>
+    </>
+  )
+}
+
+export default Feed
