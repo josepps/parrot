c97228fa1e41e556f741246a21cbdefa16a5ea89
eraldo06 <eraldoleonardo384@gmail.com>
2022-10-24 20:57:07 -0300

    perfil usuário

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,12 +1,28 @@
 import Navbar from "../componetes/Navbar/Navbar";
+import { NaoPostou } from "./Perfil.style";
 
-function Perfil(){
-    return(
+
+
+
+
+function Perfil() {
+    return (
         <>
             <Navbar/>
-            teste
-        </>
+        <NaoPostou className="feedUserTimeline">
+        <p>Você ainda não fez nenhuma publicação :(</p>
+          <img
+            className="parrotOpaco"
+            width="200px"
+            src="../..//public/img/iconeopaco.png"
+          />
+         
+        </NaoPostou>
         
+          
+           
+        </>
+
     )
 }
 
